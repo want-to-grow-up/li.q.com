b1935ac40003917a33437885b508edae86f22612
want-to-grow-up <114548878+want-to-grow-up@users.noreply.github.com>
2022-10-01 09:08:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olC2_6ufw0V1Iwzch28Sjij0LCv0',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '-d9QLvUa6e0MtE-tg8_89PoC8Y61LqcTfPydKIkKeaE',
+      useTemplateId: 'OiMRf28mWSMXRAfHyYUHpGsKNabEcxSCPVmSGHL7_Ik',
       // 所在省份
       province: '北京市',
       // 所在城市
@@ -108,7 +108,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olC2_6lNbSCrkENv4JjzD5iv2chY',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '-d9QLvUa6e0MtE-tg8_89PoC8Y61LqcTfPydKIkKeaE',
+      useTemplateId: 'OiMRf28mWSMXRAfHyYUHpGsKNabEcxSCPVmSGHL7_Ik',
       // 所在省份
       province: '北京市',
       // 所在城市
@@ -149,7 +149,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olC2_6sY1nobjE6zCsrYjD-tlCfQ',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '-d9QLvUa6e0MtE-tg8_89PoC8Y61LqcTfPydKIkKeaE',
+      useTemplateId: 'OiMRf28mWSMXRAfHyYUHpGsKNabEcxSCPVmSGHL7_Ik',
       // 所在省份
       province: '北京市',
       // 所在城市
